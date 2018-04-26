05d906806c34d6e3cf5a0534ac5c40c7aa128ed2
Vincent Bernardoff <vb@luminar.eu.org>
2018-04-26 08:45:20 +0200

    tests: switch to alcotest

--- test/test.ml
+++ test/test.ml
@@ -4,70 +4,71 @@
    %%NAME%% %%VERSION%%
   ---------------------------------------------------------------------------*)
 
-open OUnit2
 module M = Msgpck
 
 let buf = Bytes.create (5+2*0xffff)
+
+let msgpck = Alcotest.testable M.pp M.equal
 
 let wr ?(section="") ?expected size v =
   let expected = match expected with Some v -> v | None -> v in
   let nb_written = M.Bytes.write buf v in
   let computed_size = M.Bytes.size v in
-  assert_equal ~msg:(section ^ ": nb_written") ~printer:string_of_int size nb_written;
-  assert_equal ~msg:(section ^ ": size") ~printer:string_of_int nb_written computed_size;
+  Alcotest.(check int (section ^ ": nb_written") size nb_written);
+  Alcotest.(check int (section ^ ": size") nb_written computed_size);
   let nb_read, msg = M.Bytes.read buf in
-  assert_equal ~msg:(section ^ ": nb_read") ~printer:string_of_int size nb_read;
-  assert_equal expected msg
+  Alcotest.(check int (section ^ ": nb_read") size nb_read);
+  Alcotest.check msgpck (section ^ ": msgpck equality") expected msg
 
-let check ~expected buf =
-  let nb_read, msg = M.Bytes.read buf in
-  assert_equal expected msg
+let check ?(msg="") testable ~expected buf =
+  let nb_read, msgpck = M.Bytes.read buf in
+  Alcotest.check testable msg expected msgpck
 
-let negative_ints ctx =
-  check ~expected:(Int (-1)) @@ Bytes.of_string "\xff" ;
-  check ~expected:(Int (-33)) @@ Bytes.of_string "\xd0\xdf" ;
-  check ~expected:(Int (-32767)) @@ Bytes.of_string "\xd1\x80\x01" ;
-  check ~expected:(Int (-32768)) @@ Bytes.of_string "\xd2\xff\xff\x80\x00" ;
-  check ~expected:(Int (-2147483647)) @@ Bytes.of_string "\xd2\x80\x00\x00\x01" ;
-  check ~expected:(Int (-2147483648)) @@ Bytes.of_string "\xd3\xff\xff\xff\xff\x80\x00\x00\x00"
+let negative_ints () =
+  check msgpck ~expected:(Int (-1)) @@ Bytes.of_string "\xff" ;
+  check msgpck ~expected:(Int (-33)) @@ Bytes.of_string "\xd0\xdf" ;
+  check msgpck ~expected:(Int (-32767)) @@ Bytes.of_string "\xd1\x80\x01" ;
+  check msgpck ~expected:(Int (-32768)) @@ Bytes.of_string "\xd2\xff\xff\x80\x00" ;
+  check msgpck ~expected:(Int (-2147483647)) @@ Bytes.of_string "\xd2\x80\x00\x00\x01" ;
+  check msgpck ~expected:(Int (-2147483648)) @@ Bytes.of_string "\xd3\xff\xff\xff\xff\x80\x00\x00\x00"
 
-let size1 ctx =
+let size1 () =
   let l = M.[Nil; Bool true; Bool false; Int 127;
              Int (-32); Int (-31); Int (-30); Int (-2) ;Int (-1)] in
   ListLabels.iter l ~f:(wr 1)
 
-let size2 ctx =
+let size2 () =
   let l = M.[Int (-0x7f-1); Int 0xff] in
   ListLabels.iter l ~f:(wr 2)
 
-let size3 ctx =
+let size3 () =
   let l = M.[Int (-0x7fff-1); Int 0xffff] in
   ListLabels.iter l ~f:(wr 3)
 
-let size5 ctx =
+let size5 () =
   let l = M.[Some (Int Int32.(to_int max_int)), Int32 Int32.max_int;
              None, Int 0xffff_ffff
             ]
   in
   ListLabels.iter l ~f:(fun (expected, v) -> wr ?expected 5 v)
 
-let size9 ctx =
+let size9 () =
   let l = M.[None, Int64 Int64.max_int; None, Float 0.] in
   ListLabels.iter l ~f:(fun (expected, v) -> wr ?expected 9 v)
 
-let str ctx =
+let str () =
   wr ~section:"empty string" 1 @@ M.String "";
   wr 5 (M.String "Bleh");
   wr (0x20+2) (M.String (Bytes.create 0x20 |> Bytes.unsafe_to_string));
   wr (0x100+3) (M.String (Bytes.create 0x100 |> Bytes.unsafe_to_string));
   wr (0x10000+5) (M.String (Bytes.create 0x10000 |> Bytes.unsafe_to_string))
 
-let bytes ctx =
+let bytes () =
   wr (0x20+2) (M.Bytes (Bytes.create 0x20 |> Bytes.unsafe_to_string));
   wr (0x100+3) (M.Bytes (Bytes.create 0x100 |> Bytes.unsafe_to_string));
   wr (0x10000+5) (M.Bytes (Bytes.create 0x10000 |> Bytes.unsafe_to_string))
 
-let ext ctx =
+let ext () =
   wr 3 (M.Ext (4, "1"));
   wr 4 (M.Ext (4, "22"));
   wr 6 (M.Ext (4, "4444"));
@@ -81,7 +82,7 @@ let gen_list f n =
     if n > 0 then inner ((f n)::acc) (pred n) else acc
   in inner [] n
 
-let array ctx =
+let array () =
   wr ~section:"empty list" 1 @@ M.List [];
   wr ~section:"one elt" 2 M.(List [Nil]);
   wr ~section:"small array" (15+1) M.(List (gen_list (fun i -> Int i) 15));
@@ -91,29 +92,31 @@ let array ctx =
   wr ~section:"string list" 2 M.(List [String ""]);
   wr ~section:"hello wamp" 33 M.(List [Int 23; String "http://google.com"; Map [String "subscriber", Map []]])
 
-let map ctx =
+let map () =
   wr ~section:"small map" (2*15+1) M.(Map (gen_list (fun i -> Int i, Int i) 15));
   wr ~section:"medium map" (2*0xffff+3) M.(Map (gen_list (fun i -> Int 0, Int 0) 0xffff));
   wr ~section:"large map" (2*0x10000+5) M.(Map (gen_list (fun i -> Int 0, Int 0) 0x10000));
   wr ~section:"concatenated maps" 3 M.(Map [Nil, Map []]);
   wr ~section:"string -> string" 3 M.(Map [String "", String ""])
 
-let suite =
-  "msgpck" >::: [
-    "negative_ints" >:: negative_ints;
-    "size1" >:: size1;
-    "size2" >:: size2;
-    "size3" >:: size3;
-    "size5" >:: size5;
-    "size9" >:: size9;
-    "str" >:: str;
-    "bytes" >:: bytes;
-    "ext" >:: ext;
-    "array" >:: array;
-    "map" >:: map;
-  ]
+let basic = [
+  "negative_ints" , `Quick, negative_ints;
+  "size1"         , `Quick, size1;
+  "size2"         , `Quick, size2;
+  "size3"         , `Quick, size3;
+  "size5"         , `Quick, size5;
+  "size9"         , `Quick, size9;
+  "str"           , `Quick, str;
+  "bytes"         , `Quick, bytes;
+  "ext"           , `Quick, ext;
+  "array"         , `Quick, array;
+  "map"           , `Quick, map;
+]
 
-let () = run_test_tt_main suite
+let () =
+  Alcotest.run "msgpck" [
+    "basic", basic ;
+  ]
 
 (*---------------------------------------------------------------------------
    Copyright (c) 2016 Vincent Bernardoff
